223056a6ff87f03b7acd45d42225e7b6f78844e3
Vupsel2 <makandro11@gmail.com>
2023-10-24 19:33:33 +0300

    added third task

---
+++ 03_CLI_TELEGRAM_CONSOLE_SENDER/app.js
@@ -0,0 +1,32 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { program } = require('commander');
+let chatId;
+
+const token = '6496092821:AAH_kBpXFbClKXWsLNELBzOTM4N90znf0nI';
+
+const bot = new TelegramBot(token, {webhooks: true});
+
+bot.getUpdates().then((updates) => {
+    updates.forEach((update) => {
+      if (update.message) {
+        const message = update.message;
+        chatId = message.chat.id;
+        //console.log(chatId);
+    }
+    });
+    program
+    .command('m [text]')
+    .description('Send a message')
+    .action((text) => {
+      const mess=text;
+      bot.sendMessage(chatId, mess);
+    });
+    program
+    .command('p [path]')
+    .description('Send a photo')
+    .action((path) => {
+      bot.sendPhoto(chatId, path)
+    });
+    
+    program.parse();
+  });
